8ada00ed4a9271b1e8bf6148d504673c17153e6e
JungminLee97 <jungmin7618@naver.com>
2025-01-22 14:55:45 +0900

    homeWork

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TodoList from "./components/TodoList.jsx";
+
+const App = () => {
+ 
+
+  return (
+    <main className="main-center"> 
+      <TodoList />
+    </main>
+  );
+};
+
+export default App;
